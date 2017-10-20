c53acce05e1ce3525df6547dcc11652389b87fa4
degarashi <slice013@gmail.com>
2017-10-20 12:23:38 +0900

    2Dモデルのシリアライゼーションヘッダをメタヘッダにまとめた

---
+++ serialization/model2d.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "serialization/point2d.hpp"
+#include "serialization/line2d.hpp"
+#include "serialization/ray2d.hpp"
+#include "serialization/segment2d.hpp"
+#include "serialization/circle2d.hpp"
+#include "serialization/capsule2d.hpp"
+#include "serialization/aabb2d.hpp"
+#include "serialization/triangle2d.hpp"
+#include "serialization/convex2d.hpp"
--- tests/serialization.cpp
+++ tests/serialization.cpp
@@ -1,13 +1,5 @@
 #include "generate.hpp"
-#include "serialization/point2d.hpp"
-#include "serialization/line2d.hpp"
-#include "serialization/ray2d.hpp"
-#include "serialization/segment2d.hpp"
-#include "serialization/circle2d.hpp"
-#include "serialization/capsule2d.hpp"
-#include "serialization/aabb2d.hpp"
-#include "serialization/triangle2d.hpp"
-#include "serialization/convex2d.hpp"
+#include "serialization/model2d.hpp"
 
 namespace beat {
 	namespace g2 {
