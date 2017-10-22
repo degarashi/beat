6af36994d9e2a03cb34973ef770fd7b65e6a8cc6
degarashi <slice013@gmail.com>
2017-10-22 14:11:49 +0900

    geometry2d: シリアライゼーションのテスト名を変更

--- tests/serialization.cpp
+++ tests/serialization.cpp
@@ -5,6 +5,9 @@ namespace beat {
 	namespace g2 {
 		template <class T>
 		struct Serialization : Generator {};
+
+		template <class T>
+		struct SerializationG2 : Serialization<T> {};
 		using STypes = ::testing::Types<
 			Point,
 			Line,
@@ -16,9 +19,8 @@ namespace beat {
 			Triangle,
 			Convex
 		>;
-		TYPED_TEST_CASE(Serialization, STypes);
-
-		TYPED_TEST(Serialization, General) {
+		TYPED_TEST_CASE(SerializationG2, STypes);
+		TYPED_TEST(SerializationG2, General) {
 			using Mdl = Model<TypeParam>;
 			Mdl shape;
 			this->Generator::genShape(shape);
