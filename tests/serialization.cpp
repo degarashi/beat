ccef6af6712fc1f62878622f28683b961a71a7d2
degarashi <slice013@gmail.com>
2017-10-25 14:15:00 +0900

    コリジョンツリーのシリアライズコード定義をヘッダに分離

--- tests/serialization.cpp
+++ tests/serialization.cpp
@@ -1,6 +1,4 @@
-#include "generate.hpp"
-#include "tree_generate.hpp"
-#include "../serialization/tfleaf_cached.hpp"
+#include "model_serialization.hpp"
 
 namespace beat {
 	namespace g2 {
@@ -72,33 +70,3 @@ namespace beat {
 		}
 	}
 }
-
-#include <cereal/types/polymorphic.hpp>
-#include <cereal/archives/binary.hpp>
-#include <cereal/archives/json.hpp>
-
-CEREAL_REGISTER_TYPE_WITH_NAME(beat::g2::TreeGenerator::AABBNode, "AABBNode")
-CEREAL_REGISTER_TYPE_WITH_NAME(beat::g2::TreeGenerator::CircleNode, "CircleNode")
-
-CEREAL_REGISTER_TYPE_WITH_NAME(beat::g2::TreeGenerator::PointLeaf, "PointLeaf")
-CEREAL_REGISTER_TYPE_WITH_NAME(beat::g2::TreeGenerator::LineLeaf, "LineLeaf")
-CEREAL_REGISTER_TYPE_WITH_NAME(beat::g2::TreeGenerator::RayLeaf, "RayLeaf")
-CEREAL_REGISTER_TYPE_WITH_NAME(beat::g2::TreeGenerator::SegmentLeaf, "SegmentLeaf")
-CEREAL_REGISTER_TYPE_WITH_NAME(beat::g2::TreeGenerator::AABBLeaf, "AABBLeaf")
-CEREAL_REGISTER_TYPE_WITH_NAME(beat::g2::TreeGenerator::TriangleLeaf, "TriangleLeaf")
-CEREAL_REGISTER_TYPE_WITH_NAME(beat::g2::TreeGenerator::CircleLeaf, "CircleLeaf")
-CEREAL_REGISTER_TYPE_WITH_NAME(beat::g2::TreeGenerator::ConvexLeaf, "ConvexLeaf")
-CEREAL_REGISTER_TYPE_WITH_NAME(beat::g2::TreeGenerator::CapsuleLeaf, "CapsuleLeaf")
-
-CEREAL_REGISTER_POLYMORPHIC_RELATION(beat::g2::ITf, beat::g2::TreeGenerator::AABBNode)
-CEREAL_REGISTER_POLYMORPHIC_RELATION(beat::g2::ITf, beat::g2::TreeGenerator::CircleNode)
-
-CEREAL_REGISTER_POLYMORPHIC_RELATION(beat::g2::ITf, beat::g2::TreeGenerator::PointLeaf)
-CEREAL_REGISTER_POLYMORPHIC_RELATION(beat::g2::ITf, beat::g2::TreeGenerator::LineLeaf)
-CEREAL_REGISTER_POLYMORPHIC_RELATION(beat::g2::ITf, beat::g2::TreeGenerator::RayLeaf)
-CEREAL_REGISTER_POLYMORPHIC_RELATION(beat::g2::ITf, beat::g2::TreeGenerator::SegmentLeaf)
-CEREAL_REGISTER_POLYMORPHIC_RELATION(beat::g2::ITf, beat::g2::TreeGenerator::AABBLeaf)
-CEREAL_REGISTER_POLYMORPHIC_RELATION(beat::g2::ITf, beat::g2::TreeGenerator::TriangleLeaf)
-CEREAL_REGISTER_POLYMORPHIC_RELATION(beat::g2::ITf, beat::g2::TreeGenerator::CircleLeaf)
-CEREAL_REGISTER_POLYMORPHIC_RELATION(beat::g2::ITf, beat::g2::TreeGenerator::ConvexLeaf)
-CEREAL_REGISTER_POLYMORPHIC_RELATION(beat::g2::ITf, beat::g2::TreeGenerator::CapsuleLeaf)
