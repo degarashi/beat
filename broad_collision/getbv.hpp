77a30892e81b5113f29d7d5a4d3ad447f852a107
degarashi <slice013@gmail.com>
2018-02-21 09:48:25 +0900

    IGetBV: virtual-dtor fix

--- broad_collision/getbv.hpp
+++ broad_collision/getbv.hpp
@@ -5,6 +5,7 @@ namespace beat {
 	template <class BV>
 	struct IGetBV {
 		virtual BV operator()(const void* p) const = 0;
+		virtual ~IGetBV() {}
 	};
 	template <class BV>
 	using GetBV_SP = std::shared_ptr<IGetBV<BV>>;
